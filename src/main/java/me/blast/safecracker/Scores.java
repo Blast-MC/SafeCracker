6b72a6c5993686203eb43df5252395a62bbeea4d
Blast <cjblanton2@gmail.com>
2019-06-11 00:18:08 -0400

    fixed highest scores not saving

--- src/main/java/me/blast/safecracker/Scores.java
+++ src/main/java/me/blast/safecracker/Scores.java
@@ -1,10 +1,11 @@
 package me.blast.safecracker;
 
+import me.blast.safecracker.commands.Commands;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 
-public class Scores {
+public class Scores extends Commands {
 
     public Scores(long score, Player player) {
         getFiles().playerFile(player.getUniqueId()).set("score", score);
@@ -20,14 +21,13 @@ public class Scores {
                 scores.add(i, player.getDisplayName() + ": "+score);
                 break;
             }
+            if(i == scores.size()-1 && Long.parseLong(split[1]) < score){
+                scores.add(player.getDisplayName() + ": " + score);
+                break;
+            }
         }
         getFiles().scoreFile().set("scores", scores);
         getFiles().saveScore();
         SafeCracker.getInstance().log(player.getDisplayName() + " has finished the Safe Cracker event with a score of " + score + "!");
     }
-
-    public Files getFiles(){
-        return SafeCracker.getInstance().getFiles();
-    }
-
 }
